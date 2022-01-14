f05f8b4ab4c56e85ba0f77ef2970d5d1073164f0
Drew De Ponte <cyphactor@gmail.com>
2022-01-13 22:48:54 -0500

    Add inline documentation to `pipe` & `compose`
    So that not only code splunkers will be able to to get documentation but
    also so that I can setup TypeDoc documentation generation.

--- src/compose.ts
+++ src/compose.ts
@@ -1,5 +1,30 @@
+/**
+ * Composes the given functions into a function chain.
+ *
+ * The resulting composed function implements the function chain where then
+ * result of f() is passed as input to g() for which the return value of g()
+ * is return from the composed function.
+ *
+ * ```TypeScript
+ * const uppercaseAndDashSeparate = compose(uppercase, dashSeparate)
+ * uppercaseAndDashSeparate('Bob') // => 'B-O-B'
+ * ```
+ *
+ * In the above example we `compose` the `uppercase` function together with the
+ * `dashSeparate` function to create a new function called
+ * `uppercaseAndDashSeparate`. This is a contrived example but this is an
+ * extremely valuable utility. In fact it is the basis of the `pipe` function as
+ * well.
+ *
+ * @param f function of form (A) -> B
+ * @param g function of form (B) -> C
+ * @typeParam A input type for the `f` function
+ * @typeParam B input type for the `g` function
+ * @typeParam C return type for the `g` function & composed function
+ * @returns composed function implementing chain of form (A) -> C
+ */
 export function compose<A, B, C>(f: (a: A) => B, g: (b: B) => C): (a: A) => C {
   return (a: A) => {
-    return g(f(a));
-  };
+    return g(f(a))
+  }
 }
--- src/pipe.ts
+++ src/pipe.ts
@@ -1,5 +1,25 @@
-import { compose } from './compose';
+import { compose } from './compose'
 
+/**
+ * Pipes a value through a chain of functions.
+ *
+ * ```TypeScript
+ * pipe('Bob', uppercase, dashSeparate) // => 'B-O-B'
+ * ```
+ *
+ * In the above example we have a chain of two functions, `uppercase` and
+ * `dashSeparate`. The input, `'Bob'` is passed into the `uppercase` function
+ * as input and then the output from `uppercase` is piped as input to the
+ * `dashSeparate` function. Then the output from `dashSeparate` is returned as
+ * the return value of `pipe` in this example.
+ *
+ * @param f function of form (A) -> B
+ * @param g function of form (B) -> C
+ * @typeParam A input type for the `f` function & initial pipe input
+ * @typeParam B input type for the `g` function & output type of function `f`
+ * @typeParam C return type for the `g` function & `pipe`
+ * @returns output from the function chain
+ */
 export function pipe<A, B, C>(a: A, f: (a: A) => B, g: (b: B) => C): C {
-	return compose(f, g)(a);
+  return compose(f, g)(a)
 }
